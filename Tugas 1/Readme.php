9c092d48d1834719fdf8306d83c1c2e9f85f0491
Ahmad Avrizal Widiarso <80190058+ahmadavrizal@users.noreply.github.com>
2022-03-30 19:48:44 +0700

    Update Readme.php

--- Tugas 1/Readme.php
+++ Tugas 1/Readme.php
@@ -1,1 +1,6 @@
+Ahmad Avrizal Widiarso
+A11.2019.11851
+A11.4617
 
+Untuk Tugas bisa aja dilihiat melalui Link Berikut ini
+https://drive.google.com/drive/folders/1aCf0KXBUbdBV7wHrmuDXNg5WJiyhRDkf?usp=sharing
